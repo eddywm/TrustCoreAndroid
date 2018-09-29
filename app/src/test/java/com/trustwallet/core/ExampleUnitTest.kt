f4320179be9d658d10a6970b5853338096d4c6e3
Edd WM <eddywmdev@gmail.com>
2018-09-29 20:59:56 +0300

    Unit test warm-up

--- app/src/test/java/com/trustwallet/core/ExampleUnitTest.kt
+++ app/src/test/java/com/trustwallet/core/ExampleUnitTest.kt
@@ -4,14 +4,18 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+import trust.core.util.*
+
+
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun hexToInt_iSCorrect() {
+
+        val hexValue = "0xf"
+
+        val intValue = Hex.hexToInteger(hexValue)
+
+
+        assertEquals(intValue, 15)
     }
 }
